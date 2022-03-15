98438aace50a551689e5e0a714ce40211941448e
byronbyron <bwalkermills@gmail.com>
2022-03-15 22:58:12 +0000

    Set dark mode as default

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import { ThemeProvider } from 'next-themes'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeProvider enableSystem={false} attribute="class">
+    <ThemeProvider enableSystem={false} defaultTheme="dark" attribute="class">
       <Component {...pageProps} />
     </ThemeProvider>
   )
